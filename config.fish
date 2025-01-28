3e6c9bbb13ab09a12d8fed5da01328ec5ab98122
hasegawa_itsuki <ii440092@icloud.com>
2025-01-28 18:19:34 +0900

    2025 fish

--- config.fish
+++ config.fish
@@ -14,20 +14,23 @@ function peco_select_history_order
   end
 end
 
+function history
+  builtin history --show-time='%Y/%m/%d %H:%M:%S ' | sort
+end
 
 function peco_ghq
-    set -l query (commandline)
+  set -l query (commandline)
 
-    if test -n $query
-        set peco_flags --query "$query"
-    end
+  if test -n $query
+    set peco_flags --query "$query"
+  end
 
-    ghq list --full-path | peco $peco_flags | read recent
-    if [ $recent ]
-        cd $recent
-        commandline -r ''
-        commandline -f repaint
-    end
+  ghq list --full-path | peco $peco_flags | read recent
+  if [ $recent ]
+    cd $recent
+    commandline -r ''
+    commandline -f repaint
+  end
 end
 
 function fish_user_key_bindings
@@ -43,3 +46,31 @@ function fish_prompt
     end
     printf '%s %s' (set_color yellow)(prompt_pwd) $status_face
 end
+
+# aliases
+alias xcode="open -a /Applications/Xcode-16.0.0.app"
+
+# Setting PATH for Python 3.10
+# The original version is saved in /Users/toshiyukimannen/.config/fish/config.fish.pysave
+set -x PATH "/Library/Frameworks/Python.framework/Versions/3.10/bin" "$PATH"
+
+set -x GOPATH $HOME/go
+set -x PATH $PATH $GOPATH/bin
+set -x PATH  $PATH $(go env GOPATH)/bin
+set -x PATH $PATH $HOME/development/flutter/bin
+
+set -x PATH $PATH $HOME/.pub-cache/bin
+eval (direnv hook fish)
+
+set -g JAVA_HOME $JAVA_HOME /opt/homebrew/Caller/openjdk@11/11.0.19/bin
+
+set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+
+set -x PATH $PATH $HOME/.nodebrew/current/bin
+
+set -x VOLTA_HOME $VOLTA_HOME $HOME/.volta
+set -x PATH $PATH $VOLTA_HOME/bin
+set VOLTA_FEATURE_PNPM 1
+
+ln -fs /opt/homebrew/bin/gcc-14 /opt/homebrew/bin/gcc
+ln -fs /opt/homebrew/bin/g++-14 /opt/homebrew/bin/g++
