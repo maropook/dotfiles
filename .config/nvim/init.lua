6b5eedef1497a4d2fa31ffa41ad5d414044be3de
hasegawa_itsuki <ii440092@icloud.com>
2025-07-31 20:20:33 +0900

    neovimの設定を追加

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,7 +14,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   end
 end
 vim.opt.rtp:prepend(lazypath)
-
+vim.opt.clipboard = "unnamedplus"
+vim.opt.number = true
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
